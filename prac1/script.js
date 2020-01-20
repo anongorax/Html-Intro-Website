778fc704ca9f255de44b6695f86538be86e174dd
Harshdeep Saiyam <34363697+anongorax@users.noreply.github.com>
2020-01-20 17:31:45 +0530

    Add files via upload

---
+++ prac1/script.js
@@ -0,0 +1,18 @@
+//function declartion
+calculateAge(1997);
+
+function calculateAge(year){
+    console.log(2020 - year);
+}
+
+//retierment(1998);(not working)
+var retierment = function(year){
+    console.log(65 -  (2016 - year));
+}
+ retierment(1998);//right.
+
+//variables
+
+// console.log(age+"hii");
+var age = 23;
+console.log(age);
